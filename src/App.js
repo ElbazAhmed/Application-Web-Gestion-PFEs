1d156d91f7ff0e7285d3d20c7d7f0c592860e55a
Ahmed Elbaz <105916740+ElbazAhmed@users.noreply.github.com>
2023-05-08 23:22:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddPFE from './pages/AddPFE';
 import ListPFE from './pages/ListPFE';
 import MonPFE from './pages/MonPFE';
 import Registre from './registrationPage/Registre';
+import Home from '../src/componenet/Home/Home'
 
 
 function App() {
@@ -20,9 +21,15 @@ function App() {
     //     </Routes>
     //   </SideBare>
     // </BrowserRouter>
-    <div>
-      <Registre/>
-    </div>
+
+    <>
+    <BrowserRouter>
+	    <Routes>
+		    <Route index patch='/' element={<Home/>} /> 
+		    <Route patch='Registre' element={<Registre/>} /> 
+	    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
