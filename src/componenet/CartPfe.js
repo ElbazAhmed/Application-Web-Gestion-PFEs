5ee0ca7b6baaf8b229c6481f4476026983503b1b
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-05-09 01:29:14 +0100

    list pfe

---
+++ src/componenet/CartPfe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CartPfe() {
+  return (
+    <div className='border-2 border-black w-[95%] rounded-md mx-auto px-3 py-3 my-2'>
+      <h2 className='font-bold'>Problématique</h2>
+      <p>Entreprise</p>
+    </div>
+  )
+}
+
+export default CartPfe
--- src/pages/ListPFE.js
+++ src/pages/ListPFE.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../componenet/Header'
 import {TbSearch} from 'react-icons/tb'
+import CartPfe from '../componenet/CartPfe'
 
 function ListPFE() {
   return (
@@ -8,16 +9,27 @@ function ListPFE() {
       {/* componenet */}
       <Header/>
       {/* componenet */}
-      <form>
+      <form className='flex justify-end mr-7 '>
         <div className='relative flex items-center'>
-          <input placeholder='Rechercher' type='text' className='pl-5 border-2 border-black rounded-lg'/>
+          <input placeholder='Rechercher' type='text' className='pl-6 border-2 border-black rounded-lg h-8'/>
           <TbSearch className='pl-1 absolute justify-end text-sky-400 text-lg'/>
         </div>
       </form>
-      <div>
+      <div className='h-[80%]'>
         <h2 className='font-medium text-xl'>Vos offres :</h2>
-        <div>
+        <div className='w-[70%] mx-auto border-2 border-black rounded-md h-[30rem] overflow-scroll scrollbar scrollbar-thumb-sky-500 scrollbar-thin'>
+          {/* componenet */}
+          <CartPfe/>
+          <CartPfe/>
+          <CartPfe/>
+          <CartPfe/>
+          <CartPfe/>
+          <CartPfe/>
+          <CartPfe/>
+          <CartPfe/>
+          <CartPfe/>
           
+          {/* componenet */}
         </div>
       </div>
       
