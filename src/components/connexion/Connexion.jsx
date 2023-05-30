2d2278f9303c8538aaf720520ca1123058f3ecbc
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-05-30 01:59:46 +0100

    jwt login

--- src/components/connexion/Connexion.jsx
+++ src/components/connexion/Connexion.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import image from '../../assets/inpt.png'
 import {Link, Navigate} from "react-router-dom";
+import { useState } from 'react';
 
 const Connexion = () => {
-  return (
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const [redirect,setRedirect]=useState(false)
+  
+    //il faut décider par rapport au mail dans le cas d'étudiant et par les role (cord,ense)
+    
+
+    async function login(e){
+        e.preventDefault();
+        const res=await fetch('http://localhost:4000/login',{
+            method:'POST',
+            body:JSON.stringify({email,password}),
+            headers:{'Content-Type':'application/json'},
+            credentials:'include',
+        })
+        console.log(res.ok);
+        if(res.ok){
+            res.json().then((userInfo)=>{
+                console.log(userInfo);
+            })
+        }else{
+            alert('not defined')
+        }
+
+
+    }
+    return (
     <div className='flex h-[100vh]'>
         <div className='lg:w-[60%] w-[100%] flex flex-col justify-between'>
             <div className="inpt__logo">
@@ -14,11 +41,11 @@ const Connexion = () => {
                     <h2 className='text-2xl text-sky-600 font-semibold'>CONNEXION AU COMPTE</h2>
                     <p className='py-3 text-xl font-medium'>Content de te revoir! veuillez saisir vos <br /> coordonnées</p>
                 </div>
-                <form className='flex flex-col justify-center items-center gap-y-3  text-start w-[40%]'>
+                <form className='flex flex-col justify-center items-center gap-y-3  text-start w-[40%]' onSubmit={login}>
                     <div className='w-[100%] font-semibold'><h3>Adresse email :</h3></div>
-                    <input type="email" placeholder='Email' className=' border-sky-600 border-2 rounded-lg pl-2 h-9 w-[100%]'/>
+                    <input type="email" placeholder='Email' className=' border-sky-600 border-2 rounded-lg pl-2 h-9 w-[100%]' value={email} onChange={e=>setEmail(e.target.value)}/>
                     <div className='w-[100%] font-semibold'><h3>Mot de passe :</h3></div>
-                    <input type='password' placeholder='Mot de passe' className='border-sky-600 border-2 rounded-lg pl-2 h-9 w-[100%]'/>
+                    <input type='password' placeholder='Mot de passe' className='border-sky-600 border-2 rounded-lg pl-2 h-9 w-[100%]' value={password} onChange={e=>setPassword(e.target.value)}/>
                     <div className='w-[100%] text-end'>
                         <Link to='/ResetPassword'>
                             <a>Mot de passe oublié ?</a>
