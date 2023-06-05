ac9ca710887188e0dd3ec3dbff3bbff1e36645e3
Ahmed Elbaz <105916740+ElbazAhmed@users.noreply.github.com>
2023-06-05 16:20:09 +0100

    update

--- client/src/components/espaceAdmin/AddEntreprise.jsx
+++ client/src/components/espaceAdmin/AddEntreprise.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import SideBareAdmin from './SideBareAdmin';
 import Header from '../Header';
+import {Link} from "react-router-dom";
 
 const AddEntreprise = () => {
   const [nom,setNom]=useState('');
@@ -39,23 +40,25 @@ const AddEntreprise = () => {
           <h2 className='text-xl font-bold'>Ajouter une entreprise :</h2>
           <form className='flex flex-col justify-center gap-y-4 w-[75%]' onSubmit={nouveauEntreprise}>
               <input placeholder='nom' className='border-black border-2 rounded-lg pl-2 h-9'
-                value={nom} onChange={e=>setNom(e.target.value)} />
+                value={nom} onChange={e=>setNom(e.target.value)} required/>
               <input placeholder='Secteur' className='border-black border-2 rounded-lg pl-2 h-9'
-                value={secteur} onChange={e=>setSecteur(e.target.value)} />
+                value={secteur} onChange={e=>setSecteur(e.target.value)} required/>
               <input placeholder='Representant' className='border-black border-2 rounded-lg pl-2 h-9'
-                value={Representant} onChange={e=>setRepresentant(e.target.value)} />
+                value={Representant} onChange={e=>setRepresentant(e.target.value)} required/>
               <input placeholder='emailRep' className='border-black border-2 rounded-lg pl-2 h-9'
-                value={emailRep} onChange={e=>setEmailRep(e.target.value)} />
+                value={emailRep} onChange={e=>setEmailRep(e.target.value)} required/>
               <input placeholder='numeroRep' className='border-black border-2 rounded-lg pl-2 h-9'
-                value={numeroRep} onChange={e=>setNumeroRep(e.target.value)} />
+                value={numeroRep} onChange={e=>setNumeroRep(e.target.value)} required/>
               <input placeholder='Localisation' className='border-black border-2 rounded-lg pl-2 h-9'
-                value={Localisation} onChange={e=>setLocalisation(e.target.value)} />
+                value={Localisation} onChange={e=>setLocalisation(e.target.value)} required/>
               <input placeholder='email' className='border-black border-2 rounded-lg pl-2 h-9'
-                value={email} onChange={e=>setEmail(e.target.value)} />
+                value={email} onChange={e=>setEmail(e.target.value)} required/>
               <input placeholder='numero' className='border-black border-2 rounded-lg pl-2 h-9'
-                value={numero} onChange={e=>setNumero(e.target.value)} />
+                value={numero} onChange={e=>setNumero(e.target.value)} required/>
               <div className='flex gap-x-3 justify-end h-11'>
+                  <Link to='/Admin/ListEntreprises'>
                   <button className='bg-red-500 w-[20%] rounded-lg'>Annuler</button>
+                  </Link>
                   <button className='bg-green-500 w-[20%] rounded-lg' type='submit'>Ajouter</button>
               </div>
           </form>
