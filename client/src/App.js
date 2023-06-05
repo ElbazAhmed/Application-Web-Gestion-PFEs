705e8fc38ea0abbf7ba0487c62463bfc8a7e8eb6
Ahmed Elbaz <105916740+ElbazAhmed@users.noreply.github.com>
2023-06-05 02:51:32 +0100

    espace admin

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,6 @@ import MonPfeCor from './components/cordinateur/MonPfeCor';
 import ChoixPfe from './components/espaceEnseignant/ChoixPfe';
 import LayoutEnse from './components/espaceEnseignant/LayoutEnse';
 import PfeEncadrerCord from './components/cordinateur/PfeEncadrerCord';
-import AddEntreprise from './components/espaceAdmin/AddEntreprise';
 
 import {UserContextProvide} from './components/Usercontext'
 
@@ -57,7 +56,6 @@ function App() {
           {/*Espace Admin*/}
           <Route path='/EspaceAdmin' element={<Admin/>} />
           <Route path="/Admin/ListEntreprises" element={<ListEntreprises/>} />
-          <Route path="/Admin/AddEntreprise" element={<AddEntreprise/>} />
           <Route path='/Admin/Entreprise' element={<Entreprise/>} />
           <Route path="/Admin/listPfe" element={<ListPfeAdmin/>} />
           <Route path="/Admin/addPfe" element={<AddPfeAdmin/>} />
